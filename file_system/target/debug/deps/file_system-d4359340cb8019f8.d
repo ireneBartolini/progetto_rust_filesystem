d847f7ec8b9a82b62ceca87a2320e6cd8967518b
irenebartolini02 <irene.bartolini14@gmail.com>
2025-09-24 16:17:47 +0200

    struttura_fs

---
+++ file_system/target/debug/deps/file_system-d4359340cb8019f8.d
@@ -0,0 +1,5 @@
+/home/os161user/Desktop/progetto/file_system/target/debug/deps/libfile_system-d4359340cb8019f8.rmeta: src/main.rs
+
+/home/os161user/Desktop/progetto/file_system/target/debug/deps/file_system-d4359340cb8019f8.d: src/main.rs
+
+src/main.rs:
---
+++ file_system/target/debug/deps/walkdir-8ba139c3510531de.d
@@ -0,0 +1,8 @@
+/home/os161user/Desktop/progetto/file_system/target/debug/deps/libwalkdir-8ba139c3510531de.rmeta: /home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/lib.rs /home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/dent.rs /home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/error.rs /home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/util.rs
+
+/home/os161user/Desktop/progetto/file_system/target/debug/deps/walkdir-8ba139c3510531de.d: /home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/lib.rs /home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/dent.rs /home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/error.rs /home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/util.rs
+
+/home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/lib.rs:
+/home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/dent.rs:
+/home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/error.rs:
+/home/os161user/.cargo/registry/src/index.crates.io-6f17d22bba15001f/walkdir-2.5.0/src/util.rs:
