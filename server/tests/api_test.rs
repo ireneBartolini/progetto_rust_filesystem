7a7ce201083647dc15ef98ddafb96deb2d781059
Alessandro Benvenuti <alessandrobenvenuti2002@gmail.com>
2025-09-28 11:35:45 +0200

    test_list_read_write

--- server/tests/api_test.rs
+++ server/tests/api_test.rs
@@ -4,6 +4,7 @@ async fn setup() {
     // creation of some files and folders through API calls
     client.post("http://127.0.0.1:8080/mkdir/test_dir").send().await.unwrap();
     client.put("http://127.0.0.1:8080/files/test_dir/file1.txt").body("content").send().await.unwrap();
+    client.post("http://127.0.0.1:8080/mkdir/test_dir/dir1").send().await.unwrap();
 }
 
 async fn cleanup() {
@@ -12,6 +13,9 @@ async fn cleanup() {
     // delete all files and folders used for testing
     client.delete("http://127.0.0.1:8080/files/test_dir").send().await.unwrap();
 }
+
+// TESTS ON
+// GET /list/<path> – List directory contents
 
 #[tokio::test]
 #[serial_test::serial]
@@ -31,7 +35,7 @@ async fn test_list_dir_api() {
     let body: Vec<String> = res.json().await.unwrap();
     
     // assert on the body of the response
-    assert!(body.contains(&"file1.txt".to_string()));
+    assert!(body.contains(&"file1.txt".to_string()) && body.contains(&"dir1".to_string()));
 
     // cleanup
     cleanup().await
@@ -62,6 +66,9 @@ async fn test_list_dir_not_found_api() {
     cleanup().await
 }
 
+// TESTS ON
+// GET /files/<path> – Read file contents
+
 #[tokio::test]
 #[serial_test::serial]
 async fn test_file_contents() {
@@ -85,6 +92,290 @@ async fn test_file_contents() {
     // cleanup
     cleanup().await
 }
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_file_contents_not_found() {
+    // setup
+    setup().await;
+
+    // after the server is listening to 127.0.0.1:8080
+    let client = reqwest::Client::new();
+    let res = client
+        .get("http://127.0.0.1:8080/files/test_dir/file_not_found.txt")
+        .send()
+        .await
+        .unwrap();
+
+    // Check that the status is 404 not found
+    assert_eq!(res.status(), reqwest::StatusCode::NOT_FOUND);
+
+    // Check the body of the response
+    let body = res.text().await.unwrap();
+    assert!(body.contains("not found"));
+
+    // cleanup
+    cleanup().await
+}
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_file_contents_wrong_path() {
+    // setup
+    setup().await;
+
+    // after the server is listening to 127.0.0.1:8080
+    let client = reqwest::Client::new();
+    let res = client
+        .get("http://127.0.0.1:8080/files/test_dir/dir1/file1.txt")     // the file is bot in dir1
+        .send()
+        .await
+        .unwrap();
+
+    // Check that the status is 404 not found
+    assert_eq!(res.status(), reqwest::StatusCode::NOT_FOUND);
+
+    // Check the body of the response
+    let body = res.text().await.unwrap();
+    assert!(body.contains("not found"));
+
+    // cleanup
+    cleanup().await
+}
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_file_contents_directory_does_not_exist() {
+    // setup
+    setup().await;
+
+    // after the server is listening to 127.0.0.1:8080
+    let client = reqwest::Client::new();
+    let res = client
+        .get("http://127.0.0.1:8080/files/test_dir/dir2/file1.txt")     // the directory does not exist
+        .send()
+        .await
+        .unwrap();
+
+    // Check that the status is 404 not found
+    assert_eq!(res.status(), reqwest::StatusCode::NOT_FOUND);
+
+    // Check the body of the response
+    let body = res.text().await.unwrap();
+    assert!(body.contains("not found"));
+
+    // cleanup
+    cleanup().await
+}
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_file_contents_bad_request() {
+    // setup
+    setup().await;
+
+    // after the server is listening to 127.0.0.1:8080
+    let client = reqwest::Client::new();
+    let res = client
+        .get("http://127.0.0.1:8080/files/test_dir/dir1")       // trying to read the content of a directory
+        .send()
+        .await
+        .unwrap();
+
+    // Check that the status is 404 not found
+    assert_eq!(res.status(), reqwest::StatusCode::BAD_REQUEST);
+
+    // Check the body of the response
+    let body = res.text().await.unwrap();
+    assert!(body.contains("Invalid request"));
+
+    // cleanup
+    cleanup().await
+}
+
+
+// TESTS ON
+// PUT /files/<path> – Write file contents
+// TESTS ON
+// PUT /files/<path> – Write file contents
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_write_file_success() {
+    setup().await;
+
+    let client = reqwest::Client::new();
+    // Write to a new file
+    let res = client
+        .put("http://127.0.0.1:8080/files/test_dir/new_file.txt")
+        .body("hello world")
+        .send()
+        .await
+        .unwrap();
+
+    assert!(res.status().is_success());
+
+    // Read back the file to verify content
+    let res = client
+        .get("http://127.0.0.1:8080/files/test_dir/new_file.txt")
+        .send()
+        .await
+        .unwrap();
+
+    assert!(res.status().is_success());
+    let body = res.text().await.unwrap();
+    assert_eq!(body, "hello world");
+
+    cleanup().await;
+}
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_write_file_overwrite() {
+    setup().await;
+
+    let client = reqwest::Client::new();
+    // Overwrite file1.txt
+    let res = client
+        .put("http://127.0.0.1:8080/files/test_dir/file1.txt")
+        .body("new content")
+        .send()
+        .await
+        .unwrap();
+
+    assert!(res.status().is_success());
+
+    // Read back the file to verify new content
+    let res = client
+        .get("http://127.0.0.1:8080/files/test_dir/file1.txt")
+        .send()
+        .await
+        .unwrap();
+
+    assert!(res.status().is_success());
+    let body = res.text().await.unwrap();
+    assert_eq!(body, "new content");
+
+    cleanup().await;
+}
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_write_file_twice() {
+    setup().await;
+
+    let client = reqwest::Client::new();
+    // First write
+    let res = client
+        .put("http://127.0.0.1:8080/files/test_dir/file1.txt")
+        .body("first content")
+        .send()
+        .await
+        .unwrap();
+    assert!(res.status().is_success());
+
+    // Second write
+    let res = client
+        .put("http://127.0.0.1:8080/files/test_dir/file1.txt")
+        .body("second content")
+        .send()
+        .await
+        .unwrap();
+    assert!(res.status().is_success());
+
+    // Read back the file to verify it contains the second content
+    let res = client
+        .get("http://127.0.0.1:8080/files/test_dir/file1.txt")
+        .send()
+        .await
+        .unwrap();
+    assert!(res.status().is_success());
+    let body = res.text().await.unwrap();
+    assert_eq!(body, "second content");
+
+    cleanup().await;
+}
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_write_file_empty_content() {
+    setup().await;
+
+    let client = reqwest::Client::new();
+    // Write empty content to a new file
+    let res = client
+        .put("http://127.0.0.1:8080/files/test_dir/empty.txt")
+        .body("")
+        .send()
+        .await
+        .unwrap();
+
+    assert!(res.status().is_success());
+
+    // Read back the file to verify it is empty
+    let res = client
+        .get("http://127.0.0.1:8080/files/test_dir/empty.txt")
+        .send()
+        .await
+        .unwrap();
+
+    assert!(res.status().is_success());
+    let body = res.text().await.unwrap();
+    assert_eq!(body, "");
+
+    cleanup().await;
+}
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_write_file_not_found() {
+    setup().await;
+
+    let client = reqwest::Client::new();
+    // Try to write to a file in a non-existent directory
+    let res = client
+        .put("http://127.0.0.1:8080/files/test_dir/non_existant_dir/file.txt")
+        .body("should fail")
+        .send()
+        .await
+        .unwrap();
+
+    // Should return 404 not found
+    assert_eq!(res.status(), reqwest::StatusCode::NOT_FOUND);
+
+    let body = res.text().await.unwrap();
+    assert!(body.contains("not found"));
+
+    cleanup().await;
+}
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_write_file_on_directory() {
+    setup().await;
+
+    let client = reqwest::Client::new();
+    // Try to write to a directory path
+    let res = client
+        .put("http://127.0.0.1:8080/files/test_dir/dir1")
+        .body("should fail")
+        .send()
+        .await
+        .unwrap();
+
+    // Should return 400 bad request
+    assert_eq!(res.status(), reqwest::StatusCode::BAD_REQUEST);
+
+    let body = res.text().await.unwrap();
+    assert!(body.contains("Invalid request"));
+
+    cleanup().await;
+}
+
+
+
+// TESTS ON
+// POST /mkdir/<path> – Create directory
 
 #[tokio::test]
 #[serial_test::serial]
