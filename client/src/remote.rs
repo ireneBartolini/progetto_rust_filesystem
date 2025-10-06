ce24ca4b9a51dd3d6ae0d48644a3b067084e5185
irenebartolini02 <s345905@studenti.polito.it>
2025-10-06 15:37:14 +0200

    login

--- client/src/remote.rs
+++ client/src/remote.rs
@@ -1,21 +1,24 @@
 use reqwest::Client;
 use serde::Deserialize;
+use fuser::FileType;
+
+
 
 #[derive(Deserialize)]
 pub struct RemoteEntry {
     pub name: String,
-    pub kind: i32, // 1=file, 2=dir
+    pub kind: FileType, 
     pub ino: u64,
 }
 
-pub async fn list_dir(base: &str, path: &str) -> Result<Vec<RemoteEntry>, reqwest::Error> {
-    let url = format!("{}/list?path={}", base, path);
-    let resp = Client::new().get(&url).send().await?.json::<Vec<RemoteEntry>>().await?;
-    Ok(resp)
-}
+// pub async fn list_dir(base: &str, path: &str) -> Result<Vec<RemoteEntry>, reqwest::Error> {
+//     let url = format!("{}/list?path={}", base, path);
+//     let resp = Client::new().get(&url).send().await?.json::<Vec<RemoteEntry>>().await?;
+//     Ok(resp)
+// }
 
-pub async fn read_file(base: &str, path: &str) -> Result<Vec<u8>, reqwest::Error> {
-    let url = format!("{}/files?path={}", base, path);
-    let resp = Client::new().get(&url).send().await?.bytes().await?;
-    Ok(resp.to_vec())
-}
+// pub async fn read_file(base: &str, path: &str) -> Result<Vec<u8>, reqwest::Error> {
+//     let url = format!("{}/files?path={}", base, path);
+//     let resp = Client::new().get(&url).send().await?.bytes().await?;
+//     Ok(resp.to_vec())
+// }
